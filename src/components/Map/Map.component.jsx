9b59996f7668021a741971323f8a4aad7f6862be
bobothedev <minnbobo.official@gmail.com>
2023-11-29 02:29:44 +0000

    updated set view map

--- src/components/Map/Map.component.jsx
+++ src/components/Map/Map.component.jsx
@@ -1,18 +1,35 @@
 import './Map.style.css'
 import React, {useState,useEffect} from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 // import 'leaflet/dist/leaflet.css'
 import 'leaflet/dist/leaflet.css';
 const Map = ({Data}) => {
+  // if (!Data) {
+  //   return <h1>Loading</h1>
+  // }
+  const [center, setCenter] = useState([0, 0]);
+  useEffect(() => {
+    if (Data && Data.length > 0) {
+      const totalClinics = Data.length;
+      const sumLat = Data.reduce((acc, clinic) => acc + parseFloat(clinic.lat), 0);
+      const sumLng = Data.reduce((acc, clinic) => acc + parseFloat(clinic.long), 0);
+      const avgLat = sumLat / totalClinics;
+      const avgLng = sumLng / totalClinics;
+
+      setCenter([avgLat, avgLng]);
+    }
+  }, [Data]);
+  console.log('center', center)
   if (!Data) {
-    return <h1>Loading</h1>
+    return <h1>Loading</h1>;
   }
-  const totalClinics = Data?.length;
-  const sumLat = Data?.reduce((acc, clinic) => acc + parseFloat(clinic.lat), 0);
-  const sumLng = Data?.reduce((acc, clinic) => acc + parseFloat(clinic.long), 0);
-  const avgLat = sumLat / totalClinics;
-  const avgLng = sumLng / totalClinics;
-  const center =  [avgLat, avgLng];
+
+  function SetViewOnClick({ coords }) {
+    const map = useMap();
+    map.setView(coords, map.getZoom());
+  
+    return null;
+  }
 
   return (
 
@@ -33,7 +50,7 @@ const Map = ({Data}) => {
           <h5>Contact: +959 332 111 222</h5>
         </Popup>
       </Marker> */}
-
+      <SetViewOnClick coords={center} />
       {Data?.map((clinic, index) => (    
         <Marker key={index} position={[clinic.lat, clinic.long]}>
           <Popup>
